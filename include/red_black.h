5f503634d6690fb72e4e1b25748e2c23ebd13e00
Reid Paape <wrpaape@gmail.com>
2017-01-21 18:33:59 -0600

    unity test framework setup

---
+++ include/red_black.h
@@ -0,0 +1,11 @@
+#ifndef RED_BLACK_H_
+#define RED_BLACK_H_
+
+/* external dependencies
+ * ────────────────────────────────────────────────────────────────────────── */
+#include "red_black_tree.h"  /* RedBlackTree */
+#include "red_black_ltree.h" /* RedBlackLTree */
+#include "red_black_hmap.h"  /* RedBlackHMap */
+#include "red_black_lhmap.h" /* RedBlackLHmap */
+
+#endif /* ifndef RED_BLACK_H_ */
